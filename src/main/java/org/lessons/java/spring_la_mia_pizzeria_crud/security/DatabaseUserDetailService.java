f007b1e9d9d8979f021ce3686369df4076ebf4a7
MegaEmme <mikmusco@gmail.com>
2025-09-09 13:54:40 +0200

    feat: create UserDetailService for config

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/security/DatabaseUserDetailService.java
@@ -0,0 +1,36 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.security;
+
+import java.util.Optional;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.User;
+import org.lessons.java.spring_la_mia_pizzeria_crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseUserDetailService implements UserDetailsService {
+
+    // A SEGUITO DEL BLOCCO NEL CREARE SECURITY CONFIG, PER CUI HO DOVUTO CREARE
+    // USERDETAILSERVICE, MI TROVO UN ALTRO PROBLEMA, MI SERVE UN METODO CHE MI
+    // CONSENTA DI RECUPERARE LE INFO SULLA BASE DELLO USERNAME --> DEVO CREARE UNA
+    // REPOSITORY CHE IMPLEMENTI TALE METODO --> VAI A USERREPOSITORY
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> userAttempt = userRepository.findByUsername(username);
+
+        if (userAttempt.isEmpty()) {
+            throw new UsernameNotFoundException("There are no users available with username " + username);
+        }
+        // ORA DEVO RITORNARE USERDETAILS, CHE NON HO ANCORA CREATO --> CREAZIONE
+        // DATABASEUSERDETAILS
+
+    }
+}
